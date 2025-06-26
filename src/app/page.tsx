68dfe27b87d7e036c1fa3bf98011eb35a42f8386
Vitoria <vitoriamorgado65@gmail.com>
2025-06-25 22:24:11 -0300

    feat(dashboard): att2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,40 @@ import ClientForm from "./componentes/ClientForm";
 export default function Home() {
   return (
     <>
-    <ClientForm/>
+      <ClientForm />
 
-    <a href="/atividade">
-    <button
-      style={{
-      padding: "10px 24px",
-      backgroundColor: "#2563eb",
-      color: "#fff",
-      border: "none",
-      borderRadius: "6px",
-      cursor: "pointer",
-      fontSize: "16px",
-      marginTop: "16px"
-      }}
-    >
-      
-      Atividade
-    </button>
-    </a>
-    <a href="/atividade-api">
-    <button>
-      ATIVIDADE API
-    </button>
-    </a>
+      <a href="/atividade">
+        <button
+          style={{
+            padding: "10px 24px",
+            backgroundColor: "#2563eb",
+            color: "#fff",
+            border: "none",
+            borderRadius: "6px",
+            cursor: "pointer",
+            fontSize: "16px",
+            marginTop: "16px",
+          }}
+        >
+          Atividade
+        </button>
+      </a>
+      <a href="/dashboard">
+        <button
+          style={{
+            padding: "10px 24px",
+            backgroundColor: "#2563eb",
+            color: "#fff",
+            border: "none",
+            borderRadius: "6px",
+            cursor: "pointer",
+            fontSize: "16px",
+            marginTop: "16px",
+          }}
+        >
+          ATIVIDADE API
+        </button>
+      </a>
     </>
   );
 }
